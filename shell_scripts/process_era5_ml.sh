cfeedbef0c557711621c09a1a8d0e5e5ec8a5a98
Jacqueline M. Nugent <55804443+jacnugent@users.noreply.github.com>
2023-06-18 13:41:55 -0700

    Cleaned up code

--- shell_scripts/process_era5_ml.sh
+++ shell_scripts/process_era5_ml.sh
@@ -8,7 +8,6 @@ SEASON="JJA"
 SCRIPT_PATH="/home/b/b380887/cold-point-overshoot/python_scripts"
 OUT_PATH="/scratch/b/b380887/ERA5_ml/tq_regional"
 WORK_PATH="/work/bb1153/b380887/big_obs_climo"
-# GT_PATH=$OUT_PATH
 GT_PATH="/scratch/b/b380887/ERA5_ml"
 
 # set month/coord arrays to the correct season
@@ -44,39 +43,38 @@ for month in "${MonthArr[@]}"; do
     fi
     echo $month $year
     temp_gt=$GT_PATH/ERA5_T_0.25deg_ml_12-20km_$month$year"_GT.nc"
-    # tq_gt=$OUT_PATH/tq_$month$year"_GT.grib"
-    tq_gt=$GT_PATH/tq_$month$year"_GT.grib"
+    tq_gt=$OUT_PATH/tq_$month$year"_GT.grib"
     zlnsp_gt=$OUT_PATH/zlnsp_$month$year"_GT.grib"
     
     for region in "${!CoordDict[@]}"; do
         echo $region":"
-        # echo "Subsetting global tropics files..."
+        echo "Subsetting global tropics files..."
         coords="${CoordDict[$region]}"
         temp_out=$OUT_PATH/ERA5_T_0.25deg_ml_12-20km_$month$year"_"$region".nc"
         tq_out=$OUT_PATH/tq_$month$year"_"$region".grib"
         zlnsp_out=$OUT_PATH/zlnsp_$month$year"_"$region".grib"
         
-        # if [ ! -f $temp_out ] ; then
-        #     cdo sellonlatbox,$coords $temp_gt $temp_out
-        # fi
+        if [ ! -f $temp_out ] ; then
+            cdo sellonlatbox,$coords $temp_gt $temp_out
+        fi
         if [ ! -f $tq_out ] ; then
             cdo sellonlatbox,$coords $tq_gt $tq_out
         fi
-        # if [ ! -f $zlnsp_out ] ; then
-        #     cdo sellonlatbox,$coords $zlnsp_gt $zlnsp_out
-        # fi
+        if [ ! -f $zlnsp_out ] ; then
+            cdo sellonlatbox,$coords $zlnsp_gt $zlnsp_out
+        fi
         
-#         echo "Computing geopotential..."
-#         zout=$OUT_PATH/z_out_$month$year"_"$region".grib"
-#         if [ ! -f $zout ] ; then
-#             python3 $SCRIPT_PATH/compute_geopotential_on_ml.py -o $zout $tq_out $zlnsp_out
-#         fi
+        echo "Computing geopotential..."
+        zout=$OUT_PATH/z_out_$month$year"_"$region".grib"
+        if [ ! -f $zout ] ; then
+            python3 $SCRIPT_PATH/compute_geopotential_on_ml.py -o $zout $tq_out $zlnsp_out
+        fi
         
-#         echo "Converting to netcdf..."
-#         zout_nc=$OUT_PATH/ERA5_zg_0.25deg_ml_$month$year"_"$region".nc"
-#         if [ ! -f $zout_nc ] ; then
-#             grib_to_netcdf -o $zout_nc $zout
-#         fi
+        echo "Converting geopotential to netcdf..."
+        zout_nc=$OUT_PATH/ERA5_zg_0.25deg_ml_$month$year"_"$region".nc"
+        if [ ! -f $zout_nc ] ; then
+            grib_to_netcdf -o $zout_nc $zout
+        fi
 
         echo "Converting tq file to netcdf..."
         tq_out_nc=$OUT_PATH/ERA5_tq_0.25deg_ml_$month$year"_"$region".nc"
@@ -97,9 +95,7 @@ echo "Concatenating monthly files into seasonal files..."
 out_path_szn=$OUT_PATH
 for region in "${RegionArr[@]}"; do
     echo $region":"
-    # python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_T_0.25deg_ml_12-20km" -e ".nc" -r $region -m $months 
-    # python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_zg_0.25deg_ml"  -e ".nc" -r $region -m $months 
-    python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_tq-t_only_0.25deg_ml"  -e ".nc" -r $region -m $months -v "t"    
-    python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_tq-q_only_0.25deg_ml"  -e ".nc" -r $region -m $months -v "q" 
+    python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_T_0.25deg_ml_12-20km" -e ".nc" -r $region -m $months 
+    python $SCRIPT_PATH/cat_files.py -y $YEAR -f $OUT_PATH -o $out_path_szn -p "ERA5_zg_0.25deg_ml"  -e ".nc" -r $region -m $months 
 done
 
