b7fc1f231632337f06c326c70e31e08c254f09a9
Jacqueline M. Nugent <55804443+jacnugent@users.noreply.github.com>
2023-06-18 06:02:40 -0700

    Cleaned up code

--- shell_scripts/get_era5_ml.sh
+++ shell_scripts/get_era5_ml.sh
@@ -2,17 +2,16 @@
 
 module load python3
 
+source activate /home/b/b380887/.conda/envs/era5env
+
 SCRIPT_PATH="/home/b/b380887/cold-point-overshoot/python_scripts"
 OUT_PATH="/scratch/b/b380887/ERA5_ml/"
 
 COORDS="30 -180 -30 180"
 REGION="GT"
 YEAR=2010
-# declare -a MonthArr=("06" "07" "08")
-# declare -a MonthArr=("12" "01" "02")
-declare -a MonthArr=("02") # just Feb for 2010
-
-source activate /home/b/b380887/.conda/envs/era5env
+declare -a MonthArr=("12" "01" "02") # DJF
+# declare -a MonthArr=("06" "07" "08") # JJA
 
 for month in "${MonthArr[@]}"; do
     if [ "$month" == "12" ] ; then
@@ -50,25 +49,22 @@ for month in "${MonthArr[@]}"; do
     fi
     
     # only download it if it doesn't exist already
-    # if [[ ! -f $OUT_PATH/tq_zlnsp_$monthstr$year"_GT.grib" ]] ; then
-    #     python $SCRIPT_PATH/get_era5_climo_ml.py -s $date1 -e $date2 -c $COORDS -o $OUT_PATH"/" -r $REGION
-    # fi
+    if [[ ! -f $OUT_PATH/tq_zlnsp_$monthstr$year"_GT.grib" ]] ; then
+        python $SCRIPT_PATH/get_era5_climo_ml.py -s $date1 -e $date2 -c $COORDS -o $OUT_PATH"/" -r $REGION
+    fi
     
-    # TEMP - just get tq for JJA 2007/2009 and DJF 2009/2010
-    python $SCRIPT_PATH/get_era5_climo_ml.py -s $date1 -e $date2 -c $COORDS -o $OUT_PATH/ -r $REGION -v temp_and_qv
+    # split the files
+    comb_in=$GT_PATH/tq_zlnsp_$month"_GT.grib"
+    tq_out=$OUT_PATH/tq_$month$year"_GT.grib"
+    zlnsp_out=$OUT_PATH/zlnsp_$month$year"_GT.grib"
+    echo "getting tq:"
+    cdo selname,t,q $comb_in $tq_out
+    echo "getting zlnsp:"
+    cdo selname,z,lnsp $comb_in $zlnsp_out
     
-#     # split the files
-#     comb_in=$GT_PATH/tq_zlnsp_$month"_GT.grib"
-#     tq_out=$OUT_PATH/tq_$month$year"_GT.grib"
-#     zlnsp_out=$OUT_PATH/zlnsp_$month$year"_GT.grib"
-#     echo "getting tq:"
-#     cdo selname,t,q $comb_in $tq_out
-#     echo "getting zlnsp:"
-#     cdo selname,z,lnsp $comb_in $zlnsp_out
-    
-#     # remove global combined file (if everything worked)
-#     if [[ -f $tq_out && -f $zlnsp_out ]] ; then
-#         rm $comb_in
-#     fi
+    # remove global combined file (if everything worked)
+    if [[ -f $tq_out && -f $zlnsp_out ]] ; then
+        rm $comb_in
+    fi
     
 done
